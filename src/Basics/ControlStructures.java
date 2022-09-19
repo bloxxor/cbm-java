47ac12036f922dc80cd721ed685eed716e5b3c54
Florian Gabele <info@gabele-web.de>
2022-09-19 13:49:42 +0200

    + Control Structures

--- src/Basics/ControlStructures.java
+++ src/Basics/ControlStructures.java
@@ -14,33 +14,34 @@ public class ControlStructures {
 
         // Test cases
         customerName = "John Conner";
-        completePrice = 1650;
+        completePrice = 1450;
 
-        if (customerName == "John Conner") {
+        if (customerName.equals("John Conner")) {
             recurringCustomer = true;
         }
 
         if (recurringCustomer) {
 
-            System.out.println("Welcome, regular Customer! You are qualified for a discount!");
+            System.out.println("Welcome, regular Customer! You could be qualified for a discount!");
 
             if (completePrice >= 3000) {
                 System.out.println("Discount: 10%");
                 discountPrice = completePrice * 0.90;
-                System.out.println("Einkaufspreis: " + completePrice);
-                System.out.println("Rabattpreis: " + discountPrice);
             } else if (completePrice >= 2000) {
                 System.out.println("Discount: 5%");
                 discountPrice = completePrice * 0.95;
-                System.out.println("Einkaufspreis: " + completePrice);
-                System.out.println("Rabattpreis: " + discountPrice);
             } else if (completePrice >= 1500) {
                 System.out.println("Discount: 2%");
                 discountPrice = completePrice * 0.98;
-                System.out.println("Einkaufspreis: " + completePrice);
-                System.out.println("Rabattpreis: " + discountPrice);
+            } else {
+                System.out.println("You have to buy more to get a discount!");
             }
 
+            System.out.println("Price: " + completePrice);
+            System.out.println("Discount Price: " + discountPrice);
+
+        } else {
+            System.out.println("Price: " + completePrice);
         }
 
     }
