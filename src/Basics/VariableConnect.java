54f9bfa5457281b940607aeb4c8258d7a3156c44
Florian Gabele <f.gabele@bytebenders.de>
2022-09-19 11:09:01 +0200

    Variables Class

---
+++ src/Basics/VariableConnect.java
@@ -0,0 +1,17 @@
+package Basics;
+
+public class VariableConnect {
+
+    public static void main(String[] args) {
+
+        // Initialize and declare variables
+        String textPartOne = "Good morning!";
+        String textPartTwo = "Everyone!";
+        String completeSentence = textPartOne + " " + textPartTwo;
+
+        // Connect variables and print them out
+        System.out.println(completeSentence);
+
+    }
+
+}
