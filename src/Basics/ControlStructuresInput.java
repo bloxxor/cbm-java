03aaa0a57793618e0b5b146dee9a9b39a603d8fa
Florian Gabele <f.gabele@bytebenders.de>
2022-09-20 11:01:41 +0200

    Discount Calculator with user input

---
+++ src/Basics/ControlStructuresInput.java
@@ -0,0 +1,56 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class ControlStructuresInput {
+
+    public static void main(String[] args) {
+
+        // 2% bei einem Einkaufswert von mindestens 1500€,
+        // 5% bei einem Einkaufswert von mind. 2000€ und
+        // 10%, wenn der Einkaufswert mindestens bei 3000€ liegt.
+        boolean recurringCustomer = false;
+        String customerName = "";
+        double completePrice = 0.0;
+        double discountPrice = 0.0;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Name: ");
+        customerName = input.nextLine();
+
+        System.out.println("To pay: ");
+        completePrice = input.nextDouble();
+
+        if (customerName.equals("John Conner")) {
+            recurringCustomer = true;
+        }
+
+        if (recurringCustomer) {
+
+            System.out.println("Welcome, regular Customer! You could be qualified for a discount!");
+
+            if (completePrice >= 3000) {
+                System.out.println("Discount: 10%");
+                discountPrice = completePrice * 0.90;
+            } else if (completePrice >= 2000) {
+                System.out.println("Discount: 5%");
+                discountPrice = completePrice * 0.95;
+            } else if (completePrice >= 1500) {
+                System.out.println("Discount: 2%");
+                discountPrice = completePrice * 0.98;
+            } else {
+                System.out.println("You have to buy more to get a discount!");
+            }
+
+            System.out.println("Price: " + completePrice);
+            System.out.println("Discount Price: " + discountPrice);
+
+        } else {
+            System.out.println("Price: " + completePrice);
+            System.out.println("You have to buy more often, to become a recurring customer.");
+        }
+
+    }
+
+}
