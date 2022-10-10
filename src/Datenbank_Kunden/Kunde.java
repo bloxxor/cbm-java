d98dd22c02a604e209a13cb7de8c8ab08526f102
Florian Gabele <info@gabele-web.de>
2022-10-10 13:42:00 +0200

    + MySQL Connector

---
+++ src/Datenbank_Kunden/Kunde.java
@@ -0,0 +1,15 @@
+package Datenbank_Kunden;
+
+public class Kunde {
+
+    String vorname;
+    String nachname;
+    String strasse;
+
+    public Kunde(String vorname, String nachname, String strasse) {
+        this.vorname = vorname;
+        this.nachname = nachname;
+        this.strasse = strasse;
+    }
+
+}
---
+++ src/Datenbank_Kunden/Launcher.java
@@ -0,0 +1,53 @@
+package Datenbank_Kunden;
+
+import java.util.Scanner;
+
+public class Launcher {
+
+    //static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        // Erstelle Datenbankobjekt
+        //Datenbank_Kunden db = new Datenbank_Kunden();
+
+        // Einträge hinzufügen (Beispiele)
+        // Variablen Methode:
+        // Kunde kunde = new Kunde("Axel", "Schweiss", "Stinkestr. 123");
+
+//        System.out.println("Ihr Vorname: ");
+//        String vorname = scanner.nextLine();
+//
+//        System.out.println("Ihr Nachname: ");
+//        String nachname = scanner.nextLine();
+//
+//        System.out.println("Ihre Straße");
+//        String strasse = scanner.nextLine();
+//
+//        // Erstelle
+//        Kunde kunde = new Kunde(vorname, nachname, strasse);
+//        // Als Objekt übergeben
+//        db.einfuegen(kunde);
+
+//        db.loeschen(4);
+
+//        System.out.println("Ihr Vorname: ");
+//        String vorname = scanner.nextLine();
+//
+//        System.out.println("Ihr Nachname: ");
+//        String nachname = scanner.nextLine();
+//
+//        System.out.println("Ihre Straße");
+//        String strasse = scanner.nextLine();
+//
+//        System.out.println("ID");
+//        int id = scanner.nextInt();
+//
+//        // Aktualisiere
+//        Kunde kunde = new Kunde(vorname, nachname, strasse);
+//        db.aktualisieren(id, kunde);
+
+
+    }
+
+}
